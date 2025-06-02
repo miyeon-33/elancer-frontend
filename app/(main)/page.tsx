0d5bb6b1af7356860f047485a77cc3f47f542607
miyeon-33 <elav0303@gmail.com>
2025-06-02 17:34:28 +0900

    feat: 메인 두번째 섹션 완료

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,18 +1,12 @@
-import Integration from '@/app/components/home/Integration';
-import License from '@/app/components/home/License';
 import MainSlider from '@/app/components/home/MainSlider';
-import ProductList from '@/app/components/home/ProductList';
-import Promotion from '@/app/components/home/Promotion';
-import Shortcut from '@/app/components/home/Shortcut';
-import Subscribe from '@/app/components/home/Subscribe';
-import Tip from '@/app/components/home/Tip';
+import Platform from '@/app/components/home/Platform';
 
 export default function Home() {
   return (
-    <main className="max-w-[1200px] mx-auto px-[20px] h-[800px]">
+    <main className="max-w-[1200px] mx-auto h-[1800px]">
       <MainSlider />
-      {/* <ProductList />
-      <Subscribe />
+      <Platform />
+      {/* <Subscribe />
       <License />
       <Integration />
       <Promotion />
